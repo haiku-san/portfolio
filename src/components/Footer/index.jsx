92478a6e5be3d6e5152973e684965b5630b9cfe3
haiku-san <e.andaloussi.brahim@gmail.com>
2023-04-09 22:02:47 +0200

    add email to the footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,20 +4,35 @@ function Footer() {
     return (
         <footer className="footer">
             <ul>
-                <a
-                    href="https://github.com/haiku-san"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                >
-                    <li className="cta-tertiary">Github</li>
-                </a>
-                <a
-                    href="https://www.linkedin.com/in/brahim-elandaloussi/"
-                    target="_blank"
-                    rel="noreferrer noopener"
-                >
-                    <li className="cta-tertiary">Linkedin</li>
-                </a>
+                <div>
+                    <a
+                        href="https://github.com/haiku-san"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        <li className="cta-tertiary">Github</li>
+                    </a>
+                </div>
+                <div>
+                    <a
+                        href="https://www.linkedin.com/in/brahim-elandaloussi/"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        <li className="cta-tertiary">Linkedin</li>
+                    </a>
+                </div>
+                <div>
+                    <a
+                        href="mailto:elandaloussi.contact@gmail.com"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        <li className="cta-tertiary">
+                            elandaloussi.contact@gmail.com
+                        </li>
+                    </a>
+                </div>
             </ul>
         </footer>
     )
