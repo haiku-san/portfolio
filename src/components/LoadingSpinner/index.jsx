b18f196369a055c9c1c9fab06863e973b7d81a1b
haiku-san <e.andaloussi.brahim@gmail.com>
2024-04-18 21:13:16 +0200

    fix: delete src directory

--- src/components/LoadingSpinner/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function LoadingSpinner({ fullPage = false }) {
-    return <div className={`loader ${fullPage && 'full-page'}`}></div>
-}
-
-export default LoadingSpinner
